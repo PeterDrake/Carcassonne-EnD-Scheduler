b354aff804ff5d004a6f45b2dbbee9cb56977def
parulsohal <parulsohal@lclark.edu>
2015-09-10 13:26:22 -0700

    adding score and score test

---
+++ src/carc/Score.java
@@ -0,0 +1,20 @@
+package carc;
+
+public class Score {
+
+	private final int COMPCLOISTER =  9;
+	
+	public Score() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public int scoreCloister (int completed){
+		return completed * COMPCLOISTER; 
+	}
+
+}
