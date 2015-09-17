c61b520e33efd4dfc1457f33f4a0f3a0ac4c8dd2
Kian Lutu <kianlutu@lclark.edu>
2015-09-17 13:38:22 -0700

    added a way to set the number of players

--- src/carc/Carcassonne.java
+++ src/carc/Carcassonne.java
@@ -1,9 +1,20 @@
 package carc;
 
+import java.io.IOException;
+
 public class Carcassonne {
 
 	public static void main(String [] args){
-		Board b = new Board(2);
+		
+		int numberofplayers = 2;
+		System.out.print("Please Enter Number of Players: ");
+		try {
+			numberofplayers = System.in.read();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Board b = new Board(numberofplayers);
 		b.run();
 		
 		
