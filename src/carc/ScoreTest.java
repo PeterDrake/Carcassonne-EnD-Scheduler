2e3286c5798e0eb9665874a64b7395cb789210d9
Greg <gdaaronson@gmail.com>
2015-09-17 14:12:07 -0700

    Added scoring for cloisters and detection of completed ones

--- src/carc/ScoreTest.java
+++ src/carc/ScoreTest.java
@@ -2,15 +2,31 @@ package carc;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class ScoreTest {
 
-	@Test
-	public void testScoreCloister() {
-		Score s = new Score();
-		assertEquals("completed cloister scoring", 18, s.scoreCloister(2));
+	Board b;
+	Score s;
+	Player p1;
+	Player p2;
+
+	@Before
+	public void setUp() {
+		b = new Board(2);
+		s = new Score(b);
+		p1 = new Player();
 	}
 
+	@Test
+	public void testScoreCloister() {
+		b.placeTile(new Tile("nnnnlffffffff"), 10, 10);
+		s.scoreCloister(10, 10, p1);
+		assertEquals(0, p1.getScore());
+		b.placeTile(new Tile("nnnnfffffffff"), 11, 10);
+		s.scoreCloister(10, 10, p1);
+		assertEquals(1, p1.getScore());
+	}
 
 }
